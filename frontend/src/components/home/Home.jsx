e5a971d45737440934085c8a60a3ae88f5a6311c
leticia godoy <let.godoy@live.com>
2019-02-08 13:46:02 -0200

    Initial commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="inicio" subtitle="segundo projetoi de react">
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a contriicao de um cadastro desenvido com react</p>
+    </Main>
